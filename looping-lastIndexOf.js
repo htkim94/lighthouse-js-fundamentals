9c994e9eec9ba212f02fa91ff071af44ce8ea40f
Hotae Kim <hotae.kim.94@gmail.com>
2021-04-02 07:33:45 +0000

    looping-lastIndexOf challenge

---
+++ looping-lastIndexOf.js
@@ -0,0 +1,16 @@
+//looping-lastIndexOf Challenge
+let lastIndexOf = (arr, val) => {
+  let res = -1;
+  arr.forEach(function(e, i) {
+    if (e === val) {
+      res = i;
+    }
+  });
+  return res;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
